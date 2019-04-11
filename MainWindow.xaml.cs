86fb60d8d23b773b912aca68412ce66105de7cda
Peter313616 <36234285+Peter313616@users.noreply.github.com>
2019-04-11 14:52:39 -0400

    Add files via upload

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Net;
+
+namespace _313616_Sales
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        static public int monthValue = 0;
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void btn2017_Checked(object sender, RoutedEventArgs e)
+        {
+            WebClient webclient = new WebClient();
+            string download = webclient.DownloadString("https://raw.githubusercontent.com/IanMcT/April8_2019Assignment/master/2017.txt");
+            string[] Sales2017 = download.Split('\r');
+
+            if (rbIsSum.IsChecked == true)
+            {
+                decimal[] MonthlySales = new decimal[12];
+                decimal Total = 0;
+                for (int i = 1; i <= 12; i++)
+                {
+                    decimal.TryParse(Sales2017[i], out MonthlySales[i - 1]);
+
+                    Total += MonthlySales[i - 1];
+                }
+
+                decimal Average = Total / 12;
+                decimal Max = MonthlySales.Max();
+                decimal Min = MonthlySales.Min();
+
+                lblOutput.Content = "Total Annual Sales:\t" + Total.ToString()
+                    + "\nAverage Monthly Sales:\t" + Math.Round(Average, 2).ToString()
+                    + "\nMaximum Monthly Sales:\t" + Max.ToString()
+                    + "\nMinimum Monthly Sales:\t" + Min.ToString();
+                rbIsSum.IsChecked = false;
+            }
+
+            else
+            {
+                MonthPicker MP = new MonthPicker();
+                MP.ShowDialog();
+
+                lblOutput.Content = Sales2017[monthValue];
+            }
+        }
+
+        private void btn2018_Checked(object sender, RoutedEventArgs e)
+        {
+            WebClient webclient = new WebClient();
+            string download = webclient.DownloadString("https://raw.githubusercontent.com/IanMcT/April8_2019Assignment/master/2018.txt");
+            string[] Sales2018 = download.Split('\r');
+
+            if (rbIsSum.IsChecked == true)
+            {
+                decimal[] MonthlySales = new decimal[12];
+                decimal Total = 0;
+                for (int i = 1; i <= 12; i++)
+                {
+                    decimal.TryParse(Sales2018[i], out MonthlySales[i - 1]);
+
+                    Total += MonthlySales[i - 1];
+                }
+
+                decimal Average = Total / 12;
+                decimal Max = MonthlySales.Max();
+                decimal Min = MonthlySales.Min();
+
+                lblOutput.Content = "Total Annual Sales:\t" + Total.ToString()
+                    + "\nAverage Monthly Sales:\t" + Math.Round(Average,2).ToString()
+                    + "\nMaximum Monthly Sales:\t" + Max.ToString()
+                    + "\nMinimum Monthly Sales:\t" + Min.ToString();
+                rbIsSum.IsChecked = false;
+            }
+
+            else
+            {
+                MonthPicker MP = new MonthPicker();
+                MP.ShowDialog();
+
+                lblOutput.Content = Sales2018[monthValue];
+            }
+            
+        }
+    }
+}
---
+++ MonthPicker.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace _313616_Sales
+{
+    /// <summary>
+    /// Interaction logic for MonthPicker.xaml
+    /// </summary>
+    public partial class MonthPicker : Window
+    {
+        public MonthPicker()
+        {
+            InitializeComponent();
+            string[] Months = new string[12] {"January,1", "February,2", "March,3", "April,4", "May,5", "June,6", "July,7", "August,8", "Spetember,9", "October,10","November,11", "December,12" };
+            for (int i = 0; i < Months.Length; i++)
+            {
+                Button btnMonth = new Button();
+                btnMonth.Content = Months[i];
+                btnMonth.Click += BtnMonth_Click;
+                stackpanel.Children.Add(btnMonth);
+            }
+        }
+
+        private void BtnMonth_Click(object sender, RoutedEventArgs e)
+        {
+            string[] tempValue = ((Button)sender).Content.ToString().Split(',');
+            int.TryParse(tempValue[1], out MainWindow.monthValue);
+            Close();
+        }
+    }
+}
